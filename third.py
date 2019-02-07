1f81a4ab99b02fda314066d31493094fbfed767a
jasirknm <42862522+jasirknm@users.noreply.github.com>
2019-02-07 11:41:07 +0530

    third.py

--- third.py
+++ third.py
@@ -1,1 +1,8 @@
 
+char=input("enter the character to check constant or vowels:")
+if(char=='a' or char=='e' or char=='i' or char=='o' or char=='u'):
+  print("Vowels")
+elif(char=='b' or char=='c' or char=='d' or char=='f' or char=='g' or char=='h' or char=='j' or char=='k' or char=='l' or char=='m' or char=='n' or char=='p' or char=='q' or char=='r' or char=='s' or char=='t' or char=='v' or char=='w' or char=='x' or char=='y' or char=='z'):
+  print("Consonant")
+else:
+  print("invalid")
