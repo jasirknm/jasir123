a4f6fdf13046c4782d96f0c8694b002208568775
jasirknm <42862522+jasirknm@users.noreply.github.com>
2019-02-07 15:11:27 +0530

    leap.py

--- leap.py
+++ leap.py
@@ -1,5 +1,5 @@
-year = int(input())
-if (( year%400 == 0)or (( year%4 == 0 ) and ( year%100 != 0))):
+YEAR = int(input())
+if (( YEAR%400 == 0)or (( YEAR%4 == 0 ) and ( YEAR%100 != 0))):
     print("yes")
 else:
     print("no")
