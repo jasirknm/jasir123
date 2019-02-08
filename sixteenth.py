5dc74418e51665515b02b6a5ee400d2a78111e50
jasirknm <42862522+jasirknm@users.noreply.github.com>
2019-02-08 14:15:01 +0530

    sixteenth.py

--- sixteenth.py
+++ sixteenth.py
@@ -1,14 +1,13 @@
 #jasir
-a=input().split()
-n=int(a[0])
-q=int(a[1])
-for num in range(n,q):
-  temp=num
-  sum=0
-  while temp>0:
-      digit=temp%10
-      sum=sum+digit**3
-      temp=temp//10
-      if sum==num:
-           print (num)
+n,q=input().split()
+n=int(n)
+q=int(q)
+list=[]
+for num in range(n+1,q):
+	for d in range(2,num):
+		if num%d==0:
+			break
+	else:
+		list.append(num)
+print(" ".join(map(str,list)))
 #jasir
